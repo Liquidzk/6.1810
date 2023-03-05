438d340602c17c96c44b23d8ca3026ea2c3a7d71
liquidz <492928615@qq.com>
2023-03-05 22:08:36 +0800

    primes1

---
+++ user/primes.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+    int m = 2;
+    int flag = 0;
+    for (; m < 35; m++)
+    {
+        if (flag ==0 || m)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
