ed0205abb7e29083b49328de8d957f1e51c8e8da
liquidz <492928615@qq.com>
2023-03-05 21:06:13 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,16 +3,36 @@
 #include "user/user.h"
 int atoi(const char *s);
 int main(int argc, char *argv[]){
-    int i;
+    int i, pid;
+    int fd[2];
+    char buf1[10] = {0};
+    char buf2[10] = {0};
+    pipe(fd);
+    i = fork();
     if (argc != 1)
     {
-        fprintf(2,"Uncorrect sleeping. Usage: sleep [seconds]\n");
+        fprintf(2,"Uncorrect pingpong. Usage: pingpong\n");
         exit(1);
     }
-    else{
-        i = atoi(argv[0]);
-        sleep(i);
-        exit(0);
-    }
+    
+    if (!i)
+        {
+            pid = getpid();
+            while(!read(fd[1],buf1, 1));
+            fprintf(1, "%d: received ping\n", pid);
+            write(fd[0], buf1, 1);
+            exit(0);
+        }
+        else{
+            pid = getpid();
+            write(fd[0], buf2, 1);
+            wait(0);
+            if (read(fd[1],buf1, 1))
+            {
+                fprintf(1, "%d: received pong\n", pid);
+                exit(0);
+            }
+            exit(1);
+        }
     return 0;
 }
